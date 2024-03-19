5e7426afdbc4b8ecb330553b012f971e3873d19a
agvber <agvber12@gmail.com>
2024-03-19 10:07:21 +0900

    성공: 코드카타 18번

---
+++ app/src/main/java/com/agvber/algorithm_study/code_kata/18_문자열을_정수로_바꾸기.kt
@@ -0,0 +1,11 @@
+package com.agvber.algorithm_study.code_kata
+
+private fun solution(s: String): Int {
+    var answer = s.toInt()
+    return answer
+}
+
+fun main() {
+    val result = solution("1234")
+    println(result)
+}
